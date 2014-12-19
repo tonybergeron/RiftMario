a8effb5986604ed59543e981da1e975bee14e1d9
Ari Northage-Orr <arin@slalom.com>
2014-12-19 15:35:15 -0600

    Made a fireball

---
+++ Assets/shoot.js
@@ -0,0 +1,21 @@
+﻿ // The speed the bullet moves 
+var Speed : float = .3; 
+
+// The number of seconds before the bullet is automatically destroyed 
+var SecondsUntilDestroy : float = 5; 
+
+private var startTime : float; 
+
+function Start () {
+	startTime = Time.time; 
+} 
+
+function FixedUpdate () {
+	// Move up 
+	this.gameObject.transform.position += Speed * this.gameObject.transform.forward;
+
+	// If the Bullet has existed as long as SecondsUntilDestroy, destroy it 
+	if (Time.time - startTime >= SecondsUntilDestroy) {
+   		Destroy(this.gameObject);
+	} 
+}
---
+++ Assets/shootFireball.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+public var fireball : GameObject;
+public var allowfire = true;
+public var fireRate = .5;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.F)){
+		Fire();
+	}
+}
+
+function Fire () {
+	allowfire = false;
+	var newBullet : GameObject = Instantiate(fireball, transform.position + Vector3(0, 0.5, 0), transform.rotation);
+
+	yield WaitForSeconds(fireRate);
+    allowfire = true; 
+}
