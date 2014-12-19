7b768024cefc8375f415b80901127e21057e8cc7
Ari Northage-Orr <arin@slalom.com>
2014-12-19 15:07:47 -0600

    modified some scripts

---
+++ Assets/Objects/DestroyOnCollision.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+public var destroyTimer = 3;
+public var coinSound : AudioClip;
+
+function Start () {
+
+}
+
+function Update () {
+}
+
+function OnCollisionEnter(col: Collision){
+	if (!audio.isPlaying){
+		audio.clip = coinSound;
+		audio.Play();
+	}
+//	if(col.gameObject.tag == "Player"){		
+//		Destroy(this.gameObject);
+//	}
+}
